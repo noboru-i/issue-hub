7dcf3d196cdd3f5b06652f6a9cc978baf7da3973
noboru-i <ishikura.noboru@gmail.com>
2015-11-04 22:55:14 +0900

    fixed #29, #31

--- src/browser/index.js
+++ src/browser/index.js
@@ -2,6 +2,7 @@
 /*global __dirname*/
 import app from 'app';
 import BrowserWindow from 'browser-window';
+import Menu from 'menu';
 
 import ApplicationData from './libraries/application-data';
 import githubAuthUtil from './command-models/github-auth-util';
@@ -17,6 +18,30 @@ app.on('window-all-closed', () => {
 app.on('ready', () => {
   global.applicationData = new ApplicationData();
   openAuth();
+
+  var template = [
+    {
+      label: 'Application',
+      submenu: [
+          { label: 'About Application', role: 'about' },
+          { type: 'separator' },
+          { label: 'Quit', accelerator: 'Cmd+Q', click: () => { app.quit(); }}
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+          { label: 'Undo', accelerator: 'CmdOrCtrl+Z', role: 'undo' },
+          { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', role: 'redo' },
+          { type: 'separator' },
+          { label: 'Cut', accelerator: 'CmdOrCtrl+X', role: 'cut' },
+          { label: 'Copy', accelerator: 'CmdOrCtrl+C', role: 'copy' },
+          { label: 'Paste', accelerator: 'CmdOrCtrl+V', role: 'paste' },
+          { label: 'Select All', accelerator: 'CmdOrCtrl+A', role: 'selectall' }
+      ]}
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
 
 export function openAuth() {
